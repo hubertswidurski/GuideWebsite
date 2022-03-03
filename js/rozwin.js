1c0b04f69a9993275ec72ef97f0a32ec1bbad906
OwcoHodowca <65903187+OwcoHodowca@users.noreply.github.com>
2022-03-03 08:30:30 +0100

    visual fixes
    color pallete itp.

--- js/rozwin.js
+++ js/rozwin.js
@@ -12,3 +12,20 @@ function togglepotrawe(btn){
         btn.innerHTML="<img src='media/icons/line-angle-down.svg'>";
     }
 }
+function togglezabytki(btn){
+    let container = btn.parentElement.parentElement; //bierzemy diva który ma całą potawę
+    container.classList.toggle("zwiniety");//zmieniamy istnienie klasy na przeciwne
+    const kids = container.children;
+    for(let i =0; i< kids.length; i++){
+        if(!kids[i].classList.contains("cont-header"))//czy nie jest to pasek górny (wraz z przyciksiem rozwijania):
+        kids[i].classList.toggle("content-zwiniety");//jeśli nie to zwiń/rozwiń
+        else{
+            kids[i].classList.toggle("atrakcje-header-after");
+        }
+    }
+    if(btn.innerHTML.includes("down")){
+        btn.innerHTML="<img src='media/icons/line-angle-up.svg'>";
+    }else{
+        btn.innerHTML="<img src='media/icons/line-angle-down.svg'>";
+    }
+}
--- js/zabytki.js
+++ js/zabytki.js
@@ -1,7 +1,2 @@
 import {changeCountry, scrollTop} from './main.js';
-var first=true;
-document.querySelector("#anglia").addEventListener("click", ()=>{changeCountry("sec-anglia", first);first=false});
-document.querySelector("#walia").addEventListener("click", ()=>{changeCountry("sec-walia", first);first=false});
-document.querySelector("#szkocja").addEventListener("click", ()=>{changeCountry("sec-szkocja", first);first=false});
-document.querySelector("#irlandia").addEventListener("click", ()=>{changeCountry("sec-irlandia", first);first=false});
 document.querySelector("#topper").addEventListener("click", ()=> scrollTop());
